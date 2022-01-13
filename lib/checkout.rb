bd634901417422dc85786dbd9aa38c7e2fd9eea1
Sruthy V S <vsruthys@gmail.com>
2022-01-13 15:37:47 +0000

    Refactor checkout logic
    - Removed unwanted inject method
    - Change if else cases to switch case
    - Define meta data for item values
    - Define separate methods for discount calculation

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,43 +1,55 @@
+require 'pry'
+require_relative 'meta_data'
+
 class Checkout
-  attr_reader :prices
-  private :prices
-
-  def initialize(prices)
-    @prices = prices
+  def initialize(cart_items)
+    @cart_items = cart_items
   end
 
-  def scan(item)
-    basket << item.to_sym
-  end
-
-  def total
-    total = 0
-
-    basket.inject(Hash.new(0)) { |items, item| items[item] += 1; items }.each do |item, count|
-      if item == :apple || item == :pear
-        if (count % 2 == 0)
-          total += prices.fetch(item) * (count / 2)
-        else
-          total += prices.fetch(item) * count
-        end
-      elsif item == :banana || item == :pineapple
-        if item == :pineapple
-          total += (prices.fetch(item) / 2)
-          total += (prices.fetch(item)) * (count - 1)
-        else
-          total += (prices.fetch(item) / 2) * count
-        end
-      else
-        total += prices.fetch(item) * count
-      end
-    end
-
-    total
+  def checkout
+    puts "Total-#{total}"
   end
 
   private
 
-  def basket
-    @basket ||= Array.new
+  def total
+    total = 0.0
+    @cart_items.each do |cart_item|
+      case cart_item[0]
+      when :apple, :pear
+        total += buy_even_at_half_price(cart_item)
+      when :banana
+        total += buy_all_at_half_price(cart_item)
+      when :pineapple
+        total += buy_1_at_half_price(cart_item)
+      else
+        total += price_of(cart_item) * cart_item[1]
+      end
+    end
+    total
+  end
+
+  def price_of(cart_item)
+    MetaData::ITEMS.select do |item|
+      item[:name].eql?(cart_item[0].to_s)
+    end.first[:price]
+  end
+
+  def buy_even_at_half_price(cart_item)
+    count = cart_item[1]
+    return price_of(cart_item) * (count / 2) if (count % 2 == 0)
+
+    price_of(cart_item) * count
+  end
+
+  def buy_1_at_half_price(cart_item)
+    half_price = price_of(cart_item) / 2
+    (price_of(cart_item) * (cart_item[1] - 1)) + half_price
+  end
+
+  def buy_all_at_half_price(cart_item)
+    (price_of(cart_item) / 2) * cart_item[1]
   end
 end
+
+Checkout.new({'apple': 3, 'pineapple': 4, 'banana': 5, 'pear': 2, 'mango': 3}).checkout
---
+++ lib/meta_data.rb
@@ -0,0 +1,24 @@
+class MetaData
+  ITEMS = [
+    {
+      name: 'apple',
+      price: 1.0
+    },
+    {
+      name: 'pear',
+      price: 2.0
+    },
+    {
+      name: 'pineapple',
+      price: 3.0
+    },
+    {
+      name: 'banana',
+      price: 4.0
+    },
+    {
+      name: 'mango',
+      price: 5.0
+    }
+  ]
+end
