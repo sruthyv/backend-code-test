09b64c9bb436209a8f4af11f410385b1ee1e13d4
sruthyv <vsruthys@gmail.com>
2022-01-25 15:11:59 +0000

    Update the variable names of discount calculator service

--- services/discount_calculator.rb
+++ services/discount_calculator.rb
@@ -2,9 +2,9 @@ class DiscountCalculator
   def initialize(cart_item:)
     @price = ::MetaData::PRICING_RULES.fetch(cart_item[:item])
     @quantity = cart_item[:quantity]
-    @cart_item = cart_item[:item]
+    @item_name = cart_item[:item]
     @discount = cart_item[:discount]
-    @cart_item_hash = cart_item
+    @cart_item = cart_item
   end
   
   def call
@@ -25,32 +25,32 @@ class DiscountCalculator
   private
 
   def buy_even_at_half_price
-    return 0.0 unless ( @cart_item == :apple || @cart_item == :pear )
+    return 0.0 unless ( @item_name == :apple || @item_name == :pear )
     return @price * @quantity unless (@quantity % 2 == 0)
 
     @price * (@quantity / 2)
   end
 
   def buy_all_at_half_price
-    return 0.0 unless @cart_item == :banana
+    return 0.0 unless @item_name == :banana
 
     (@price / 2) * @quantity
   end
 
   def buy_1_at_half_price
-    return 0.0 unless @cart_item == :pineapple
+    return 0.0 unless @item_name == :pineapple
 
     (@price * (@quantity - 1)) + (@price / 2)
   end
 
   def buy_three_get_one
-    return 0.0 unless @cart_item == :mango
+    return 0.0 unless @item_name == :mango
 
-    @cart_item_hash[:quantity] = ((@quantity / 3) + @quantity) # Increare Item quantity
+    @cart_item[:quantity] = ((@quantity / 3) + @quantity) # Increare Item quantity
     @price * ( @quantity - (@quantity / 3))
   end
 
   def scan_price(total)
-    @cart_item_hash.merge(total: total)
+    @cart_item.merge(total: total)
   end
 end
